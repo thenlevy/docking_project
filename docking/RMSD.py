a809360a91dbc986414215d25e52641129b83338
thenlevy <nicolaspierrelevy@gmail.com>
2018-05-14 17:50:09 +0200

    Make rmsd computation pypy friendly

--- docking/RMSD.py
+++ docking/RMSD.py
@@ -1,22 +1,19 @@
-import numpy as np
 import parser_pdb as pdb
 import sys
 
-file1 = sys.argv[1]
-#file2 = sys.argv[2]
-#file3 = sys.argv[3]
 
 
 # calcule le rmsd a partir d'une liste de distance entre atomes
 def rmsd(distances):
-    return np.sqrt((np.array(distances) ** 2).mean())
+    n = len(distances)
+    return (sum(map(lambda x: x ** 2, distances)) / n) ** 0.5
 
 # prends 2 liste des coordonnes des atomes CA concatenee a la suite (x,y,z,x,y,z,...)
 # renvoi une liste des distances
 def distances(CA1, CA2):
 	distanceList = []
 	for i in range(0,len(CA1),3):
-		distanceList.append(np.sqrt((CA2[i]-CA1[i])**2+(CA2[i+1]-CA1[i+1])**2+(CA2[i+2]-CA1[i+2])**2))
+		distanceList.append(((CA2[i]-CA1[i])**2+(CA2[i+1]-CA1[i+1])**2+(CA2[i+2]-CA1[i+2])**2) ** 0.5)
 	return distanceList
 
 
