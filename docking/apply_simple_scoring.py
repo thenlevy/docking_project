ee322fa94ab2ee909c42a5175d775845c901e0da
thenlevy <nicolaspierrelevy@gmail.com>
2018-05-14 22:30:34 +0200

    Add rmsd to scoring and results

--- docking/apply_simple_scoring.py
+++ docking/apply_simple_scoring.py
@@ -1,9 +1,11 @@
 from Cornell import comp_score
+from RMSD import rmsdLigant
 import os
 from shutil import copyfile
 
 scores = []
 score_map = { }
+rmsd_map = { }
 for pdb_file in os.listdir("../data"):
     # We want to exclude the example files from the analysis
     if (pdb_file.find("natif") == -1 and pdb_file != 'ex.pdb'
@@ -12,15 +14,20 @@ for pdb_file in os.listdir("../data"):
         score = comp_score("../data/Rec_natif.pdb", "../data/" + pdb_file)
         scores.append(score)
         score_map[score] = pdb_file
+        rmsd_map[score] = rmsdLigant("../data/Lig_natif.pdb", "../data/" + pdb_file)
 
 if not os.path.exists("../data/scoring_Cornell"):
     os.mkdir("../data/scoring_Cornell")
 
 ranking = open("../data/scoring_Cornell/ranking.txt", 'w')
+rank_rmsd = open("../data/scoring_Cornell/score_and_rmsd.csv", 'w')
+
+rank_rmsd.write("file;score;rmsd\n")
 scores.sort()
 rank = 1
 for score in scores:
     ranking.write("rank {} : {} with score {}\n".format(rank, score_map[score], score))
+    rank_rmsd.write("{};{};{}\n".format(score_map[score], score, rmsd_map[score]))
     rank += 1
 
 best_pdb = score_map[scores[0]]
